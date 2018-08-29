4448d10cbb74189907828898e8f15c48ad4c42e3
Andrey Gusakov <andrey.gusakov@cogentembedded.com>
2018-08-29 17:56:48 +0300

    net: mv88e6xxx: add eeprom and internal mdio bus support

--- drivers/net/phy/mv88e6xxx.c
+++ drivers/net/phy/mv88e6xxx.c
@@ -4,6 +4,11 @@
 #include <linux/ethtool.h>
 #include <linux/phy.h>
 
+/* sub-devices MDIO addresses */
+#define MV88E6XXX_SWITCH_GLOBAL_REGS_1		0x1b
+#define MV88E6XXX_SWITCH_GLOBAL_REGS_2		0x1c
+#define MV88E6XXX_SWITCH_GLOBAL_REGS_3		0x1d
+
 /* Offset 0x00: Port Status Register */
 #define MV88E6XXX_PORT_STS			0x00
 #define MV88E6XXX_PORT_STS_PAUSE_EN		0x8000
@@ -358,6 +363,19 @@ static const struct mv88e6xxx_info mv88e6xxx_table[] = {
 	},
 };
 
+struct mv88e6xxx_priv {
+	/* common */
+	struct mii_bus		*parent_miibus;
+	const struct mv88e6xxx_info	*info;
+
+	/* internal mii bus */
+	struct mii_bus 		miibus;
+
+	/* eeprom */
+	struct cdev		cdev;
+	struct cdev_operations	fops;
+};
+
 static int mv88e6xxx_port_config_init(struct phy_device *phydev)
 {
 	int mac_ctl, err;
@@ -439,6 +457,254 @@ static struct phy_driver mv88e6xxx_port_driver = {
 	.read_status	= mv88e6xxx_port_read_status,
 };
 
+/* common helpers for eerom and internal mii bus */
+static int mv88e6xxx_read_reg(struct mv88e6xxx_priv *priv, int addr, int regnum)
+{
+	return mdiobus_read(priv->parent_miibus, addr, regnum);
+}
+
+static int mv88e6xxx_write_reg(struct mv88e6xxx_priv *priv, int addr, int regnum, u16 value)
+{
+	return mdiobus_write(priv->parent_miibus, addr, regnum, value);
+}
+
+/* internal MDIO bus stuff */
+static int mv88e6xxx_miibus_read(struct mii_bus *bus, int phy_id, int regnum)
+{
+	u16 reg;
+	int status;
+	int timeout = 100;
+	struct mv88e6xxx_priv *priv = bus->priv;
+
+	status = mv88e6xxx_read_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x18);
+	if (status < 0)
+		return status;
+	if (status & (1 << 15))
+		return -EBUSY;
+
+	reg  = regnum & 0x1f;
+	reg |= (phy_id & 0x1f) << 5;
+	reg |= (0x2 << 10);	/* read cmd */
+	reg |= (1 << 12);	/* Clause 22 */
+	reg |= (1 << 15);	/* start */
+
+	status = mv88e6xxx_write_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x18, reg);
+
+	do {
+		status = mv88e6xxx_read_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x18);
+	} while ((--timeout) && (status > 0) && (status & (1 << 15)));
+	if (timeout == 0)
+		return -ETIMEDOUT;
+	if (status < 0)
+		return status;
+
+	/* read data */
+	status = mv88e6xxx_read_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x19);
+
+	return status;
+}
+
+static int mv88e6xxx_miibus_write(struct mii_bus *bus, int phy_id,
+			       int regnum, u16 val)
+{
+	u16 reg;
+	int status;
+	int timeout = 100;
+	struct mv88e6xxx_priv *priv = bus->priv;
+
+	status = mv88e6xxx_read_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x18);
+	if (status < 0)
+		return status;
+	if (status & (1 << 15))
+		return -EBUSY;
+
+	/* write data */
+	status = mv88e6xxx_write_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x19, val);
+	if (status < 0)
+		return status;
+
+	reg  = regnum & 0x1f;
+	reg |= (phy_id & 0x1f) << 5;
+	reg |= (0x1 << 10);	/* write cmd */
+	reg |= (1 << 12);	/* Clause 22 */
+	reg |= (1 << 15);	/* start */
+
+	status = mv88e6xxx_write_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x18, reg);
+
+	do {
+		status = mv88e6xxx_read_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x18);
+	} while ((--timeout) && (status > 0) && (status & (1 << 15)));
+	if (timeout == 0)
+		return -ETIMEDOUT;
+
+	return status;
+}
+
+/* EEPROM stuff */
+static int mv88e6xxx_read_eeprom16(struct mv88e6xxx_priv *priv, size_t off)
+{
+	u16 reg;
+	int status;
+	int timeout = 100;
+
+	/* prepare cmd */
+	reg  = (off & 0xff);
+	reg |= (0x4 << 12);	/* read cmd */
+	reg |= (1 << 15);	/* start operation */
+	status = mv88e6xxx_write_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x14, reg);
+	if (status < 0)
+		return status;
+	do {
+		status = mv88e6xxx_read_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x14);
+	} while ((--timeout) && (status > 0) && (status & (1 << 15)));
+	if (timeout == 0)
+		return -ETIMEDOUT;
+	if (status < 0)
+		return status;
+	return mv88e6xxx_read_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x15);
+}
+
+static int mv88e6xxx_write_eeprom16(struct mv88e6xxx_priv *priv, size_t off, u16 value)
+{
+	u16 reg;
+	int status;
+	int timeout = 100;
+
+	/* prepare data */
+	status = mv88e6xxx_write_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x15, value);
+	if (status < 0)
+		return status;
+	/* prepare cmd */
+	reg  = (off & 0xff);
+	reg |= (0x3 << 12);	/* write cmd */
+	reg |= (1 << 15);	/* start operation */
+	status = mv88e6xxx_write_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x14, reg);
+	if (status < 0)
+		return status;
+	do {
+		status = mv88e6xxx_read_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x14);
+	} while ((--timeout) && (status > 0) && (status & (1 << 15)));
+	if (timeout == 0)
+		return -ETIMEDOUT;
+
+	return 0;
+}
+
+static ssize_t mv88e6xxx_eeprom_cdev_read(struct cdev *cdev, void *_buf, size_t count,
+		loff_t off, ulong flags)
+{
+	int status;
+	ssize_t retval = 0;
+	unsigned char *buf = _buf;
+	struct mv88e6xxx_priv *priv = cdev->priv;
+
+	if (unlikely(!count))
+		return count;
+
+	/* check if eeprom is available */
+	status = mv88e6xxx_read_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x14);
+	if (status < 0)
+		return status;
+	if ((status & (1 << 11)) || (status & (1 << 15))) {
+		dev_err(cdev->dev, "EEPROM interface busy: 0x%04x\n", status);
+		return -ETIMEDOUT;
+	}
+
+	while (count) {
+		int min;
+
+		if ((count == 1) || (off & 1))
+			min = 1;
+		else
+			min = 2;
+
+		status = mv88e6xxx_read_eeprom16(priv, off >> 1);
+		if (status < 0)
+			return status;
+
+		/* store data */
+		if (min == 2) {
+			buf[0] = status & 0xff;
+			buf[1] = (status >> 8) & 0xff;
+		} else if (off & 1) {
+			/* first unaligned byte */
+			buf[0] = (status >> 8) & 0xff;
+		} else {
+			/* last unaligned byte */
+			buf[0] = status & 0xff;
+		}
+
+		/* adjust pointer and counters */
+		buf += min;
+		off += min;
+		count -= min;
+		retval += min;
+	}
+
+	return retval;
+}
+
+static ssize_t mv88e6xxx_eeprom_cdev_write(struct cdev *cdev, const void *_buf, size_t count,
+		loff_t off, ulong flags)
+{
+	int status;
+	ssize_t retval = 0;
+	const unsigned char *buf = _buf;
+	struct mv88e6xxx_priv *priv = cdev->priv;
+
+	if (unlikely(!count))
+		return count;
+
+	/* check if eeprom is available */
+	status = mv88e6xxx_read_reg(priv, MV88E6XXX_SWITCH_GLOBAL_REGS_2, 0x14);
+	if (status < 0)
+		return status;
+	if ((status & (1 << 11)) || (status & (1 << 15))) {
+		dev_err(cdev->dev, "EEPROM interface busy: 0x%04x\n", status);
+		return -ETIMEDOUT;
+	}
+	if (!(status & (1 << 10))) {
+		dev_err(cdev->dev, "EEPROM write disabled\n");
+		return -EIO;
+	}
+
+	while (count) {
+		int min;
+		u16 value;
+
+		/* prepare data */
+		if ((count == 1) || (off & 1)) {
+			min = 1;
+			value = mv88e6xxx_read_eeprom16(priv, off >> 1);
+			if (value < 0)
+				return value;
+		} else {
+			min = 2;
+		}
+
+		if (min == 2)
+			value  = buf[0] | (buf[1] << 8);
+		else if (off & 1)
+			/* first unaligned byte */
+			value = (value & 0x00ff) | (buf[0] << 8);
+		else
+			/* last unaligned byte */
+			value = (value & 0xff00) | (buf[0] << 0);
+
+		status = mv88e6xxx_write_eeprom16(priv, off >> 1, value);
+		if (status < 0)
+			return status;
+
+		/* adjust pointer and counters */
+		buf += min;
+		off += min;
+		count -= min;
+		retval += min;
+	}
+
+	return retval;
+}
+
 static const struct mv88e6xxx_info *mv88e6xxx_lookup_info(unsigned int prod_num)
 {
 	int i;
@@ -478,20 +744,40 @@ static const struct mv88e6xxx_info *mv88e6xxx_detect(struct device_d *dev,
 
 static int mv88e6xxx_probe(struct device_d *dev)
 {
-	struct device_node *switch_node, *port_node;
-	const struct mv88e6xxx_info *info;
-	struct mii_bus *mdio_bus;
+	int err;
+	u32 eeprom_size;
+	struct device_node *switch_node, *port_node, *mdio_node;
+	struct mv88e6xxx_priv *priv;
 
-	mdio_bus = of_mdio_find_bus(dev->device_node->parent);
-	if (!mdio_bus)
+	priv = xzalloc(sizeof(struct mv88e6xxx_priv));
+
+	priv->parent_miibus = of_mdio_find_bus(dev->device_node->parent);
+	if (!priv->parent_miibus)
 		return -EPROBE_DEFER;
 
-	info = mv88e6xxx_detect(dev, mdio_bus);
-	if (IS_ERR(info)) {
+	priv->info = mv88e6xxx_detect(dev, priv->parent_miibus);
+	if (IS_ERR(priv->info)) {
 		dev_err(dev, "Error: Failed to detect switch type\n");
-		return PTR_ERR(info);
+		return PTR_ERR(priv->info);
 	}
 
+	/* internal MII bus first as ports may want to attach to phys on this bus */
+	mdio_node = of_find_node_by_name(dev->device_node, "mdio");
+	if (mdio_node) {
+		priv->miibus.read = mv88e6xxx_miibus_read;
+		priv->miibus.write = mv88e6xxx_miibus_write;
+		/* only first 5 are phys */
+		priv->miibus.phy_mask = 0xffffffe0;
+		priv->miibus.priv = priv;
+		priv->miibus.parent = dev;
+		priv->miibus.dev.device_node = mdio_node;
+
+		mdiobus_register(&priv->miibus);
+	} else {
+		dev_err(dev, "No MDIO node\n");
+	}
+
+	/* register ports */
 	switch_node = of_find_node_by_name(dev->device_node, "ports");
 	if (!switch_node)
 		return -EINVAL;
@@ -509,14 +795,15 @@ static int mv88e6xxx_probe(struct device_d *dev)
 			continue;
 		}
 
-		if (nr >= info->num_ports) {
+		if (nr >= priv->info->num_ports) {
 			dev_err(dev, "Error: Incorrect port number %d\n", nr);
 			continue;
 		}
 
-		phydev = phy_device_create(mdio_bus,
-					   info->port_base_addr + nr, 0);
+		phydev = phy_device_create(priv->parent_miibus,
+					   priv->info->port_base_addr + nr, 0);
 		phydev->dev.driver      = &mv88e6xxx_port_driver.drv;
+		phydev->dev.parent	= &priv->parent_miibus->dev;
 		phydev->dev.device_node = port_node;
 		phydev->autoneg         = AUTONEG_DISABLE;
 
@@ -524,6 +811,36 @@ static int mv88e6xxx_probe(struct device_d *dev)
 		if (r) {
 			dev_err(dev, "Error: Failed to register a PHY\n");
 		}
+	}
+
+	/* EEPROM */
+	if (/* TODO: check for eeprom access support in switch */
+	    of_property_read_u32(dev->device_node, "eeprom-length", &eeprom_size)) {
+		char *devname;
+		const char *alias;
+
+		alias = of_alias_get(dev->device_node);
+		if (alias) {
+			devname = xstrdup(alias);
+		} else {
+			err = cdev_find_free_index("eeprom");
+			if (err < 0) {
+				dev_err(dev, "no index found to name EEPROM device\n");
+				return err;
+			}
+			devname = xasprintf("eeprom%d", err);
+		}
+
+		priv->cdev.name = devname;
+		priv->cdev.priv = priv;
+		priv->cdev.dev = dev;
+		priv->cdev.ops = &priv->fops;
+		priv->cdev.size = eeprom_size;
+		priv->fops.lseek = dev_lseek_default;
+		priv->fops.read	= mv88e6xxx_eeprom_cdev_read;
+		priv->fops.write = mv88e6xxx_eeprom_cdev_write;
+
+		err = devfs_create(&priv->cdev);
 	}
 
 	return 0;
